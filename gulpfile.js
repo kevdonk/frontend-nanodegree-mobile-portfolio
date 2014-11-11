8c711a2845859fa72829ce0df01dd1a021ec921f
kevdonk <kevin@mayoind.com>
2014-11-10 18:58:48 -0700

    optimize

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,43 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
-var minify = require('gulp-minify-css');
-var psi = require('psi');
+var cssmin = require('gulp-cssmin');
+var uncss = require('gulp-uncss');
+var imgmin = require('gulp-imagemin');
+var htmlmin = require('gulp-htmlmin');
 var deploy = require('gulp-gh-pages');
-var site = 'http://127.0.0.1:8000';
 
+gulp.task('html', function() {
+	return gulp.src('*.html')
+	.pipe(htmlmin({collapseWhitespace: true}))
+	.pipe(gulp.dest('dist'));
+});
 
-gulp.task('psi', function(cb) {
-	psi({
-		nokey: 'true',
-		url: site,
-		strategy: 'mobile',
-	}, cb);
+gulp.task('png', function() {
+	return gulp.src('img/*.png')
+	.pipe(imgmin())
+	.pipe(gulp.dest('dist/img/'));
+});
+
+gulp.task('jpg', function() {
+	return gulp.src('img/*.jpg')
+	.pipe(imgmin())
+	.pipe(gulp.dest('dist/img/'));
 });
 
 gulp.task('scripts', function() {
 	return gulp.src('js/*.js')
-	.pipe(concat('all.min.js'))
+	.pipe(concat('perfmatters.js'))
 	.pipe(uglify())
-	.pipe(gulp.dest('dist'));
+	.pipe(gulp.dest('dist/js/'));
 });
+
 
 gulp.task('css', function() {
 	return gulp.src('css/*.css')
-	.pipe(concat('all.min.css'))
-	.pipe(minify())
-	.pipe(gulp.dest('dist'));
+	.pipe(uncss({html: '*.html'}))
+	.pipe(cssmin())
+	.pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('jshint', function() {
@@ -44,6 +55,9 @@ gulp.task('deploy', function() {
 gulp.task('watch', function() {
 	gulp.watch('js/*.js') ['scripts'];
 	gulp.watch('css/*.css') ['css'];
+	gulp.watch('*.html') ['html'];
 });
 
-gulp.task('default', ['jshint','scripts','css','watch']);
+gulp.task('build', ['jshint', 'scripts','css','html', 'png','jpg']);
+
+gulp.task('default', ['build','watch']);
